72dea20df76d22654fe6c019c123746e38179ef0
oyaai <oyahidden@gmail.com>
2018-06-07 05:33:28 +0700

    Update _con.php

--- libs/_con.php
+++ libs/_con.php
@@ -1,11 +1,16 @@
 <?
 // connect 06-06-2018
-$dsn = "localhost";
+$dsn = "mysql:dbname=umarin2_egn;host=localhost";
 $dbuser = "umarin2_oya";
 $dbpwd = "oya@1925";
 
+$url = 'http://pacificprime.egnserv.com/';
+
 $conn = new PDO($dsn, $dbuser, $dbpwd);
-$sql = "INSERT INTO egn_pacific VALUES('','$_REQUEST['name']','$_REQUEST['phone']','$_REQUEST['email']','$_REQUEST['company']','$_REQUEST['file']');";	
+$sql = "INSERT INTO egn_pacific VALUES('','".$_REQUEST['name']."','".$_REQUEST['phone']."','".$_REQUEST['email']."','".$_REQUEST['company']."','".$_REQUEST['file']."');";	
+echo $sql;
+break;
+
 $result = $conn->prepare($sql);
 
 $status = $result->execute();
