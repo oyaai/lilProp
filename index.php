5f59dbc898fac8c9d80b5c44968334c97b7c5214
oyaai <oyahidden@gmail.com>
2018-06-14 04:49:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -77,13 +77,31 @@
 <script>
 $(document).ready(function(){
 	setInterval(function(){
+
+		var getUrlParameter = function getUrlParameter(sParam) {
+		var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+			sURLVariables = sPageURL.split('&'),
+			sParameterName,
+			i;
+
+		for (i = 0; i < sURLVariables.length; i++) {
+			sParameterName = sURLVariables[i].split('=');
+
+			if (sParameterName[0] === sParam) {
+				return sParameterName[1] === undefined ? true : sParameterName[1];
+				}
+			}
+		};
+		
+		var $p = getUrlParameter('p');
+
 		if($p == "back"){
 			$("#txtTest").load("/libs/result.php");
 		}else{
 			$("#txtTest").load("Result");
 			
 		}
-	}, 3000);
+	},3000);
 });
 </script>
 </body>
